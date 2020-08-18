b4947a8095c6f880a9da23dedb68dee0d6336ed5
Chris Chuang <chrischuang0421@gmail.com>
2020-08-19 00:12:27 +0800

    Code refined.

--- src/comps/ProgressBar.tsx
+++ src/comps/ProgressBar.tsx
@@ -2,20 +2,20 @@ import React, { FC, useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
 type Props = {
-  file: File | null;
+  file: File;
   setFile: React.Dispatch<React.SetStateAction<File | null>>;
 };
 
 const ProgressBar: FC<Props> = ({ file, setFile }) => {
   // console.log("render");
-  const { progress, url, error } = useStorage(file);
+  const { progress, url } = useStorage(file);
   console.log(progress, url);
 
   useEffect(() => {
     if (url) {
       setFile(null);
     }
-  }, [url]);
+  }, [url, setFile]);
 
   return <div className="progress-bar" style={{ width: `${progress}%` }}></div>;
 };
--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -6,36 +6,34 @@ import {
 } from "../firebase/config";
 import * as firebase from "firebase";
 
-const useStorage = (file: File | null) => {
+const useStorage = (file: File) => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState<Error | null>(null);
   const [url, setUrl] = useState<string | null>(null);
 
   useEffect(() => {
-    if (file) {
-      // upload refrerences
-      const storageRef = projectStorage.ref(file.name);
-      const collectionRef = projectFireStore.collection("images");
+    // upload refrerences
+    const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFireStore.collection("images");
 
-      // upload
-      storageRef.put(file).on(
-        firebase.storage.TaskEvent.STATE_CHANGED,
-        (snap) => {
-          const percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-          // console.log("SET!");
-          setProgress(percentage);
-        },
-        (err) => {
-          setError(err);
-        },
-        async () => {
-          const url = await storageRef.getDownloadURL();
-          const createdAt = timestamp();
-          await collectionRef.add({ url, createdAt });
-          setUrl(url);
-        }
-      );
-    }
+    // upload
+    storageRef.put(file).on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      (snap) => {
+        const percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        // console.log("SET!");
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        await collectionRef.add({ url, createdAt });
+        setUrl(url);
+      }
+    );
   }, [file]);
 
   return { progress, url, error };
